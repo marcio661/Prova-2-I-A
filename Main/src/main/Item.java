80c3656f5e0e6730fad32033258f0b6ecc8fe9ce
Marcio Luis Cipriano <62039843+marcio661@users.noreply.github.com>
2023-08-17 09:50:07 -0300

    Add files via upload

---
+++ Main/src/main/Item.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author Marcio Luis & Marilene Rodrigues
+ */
+// Classe para representar um item da mochila
+class Item {
+    int weight;
+    int value;
+
+    public Item(int weight, int value) {
+        this.weight = weight;
+        this.value = value;
+    }
+}
